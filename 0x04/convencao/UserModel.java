9fa624c003fe0aaec82733b77b383dee38dddd37
Romário Vieira <romario.sistemas@gmail.com>
2025-10-22 23:34:49 -0300

    convencao

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,14 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+private int idUser;
+
+private String descricao;
+
+final static int IDADEMINIMA = 18;
+
+// função que coleta todos os users
+public GetUsers() {return [];}
+
+}
