804808e30347d72fc441cc34daa7240f60c8a931
Romário Vieira <romario.sistemas@gmail.com>
2025-10-22 23:23:16 -0300

    correcao

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,8 +1,5 @@
 public class Comments {
 
-    // Copyright (C) 2022 by CleanCode.
-    // Lançado sob os termos da GNU General Public License versão 2 ou posterior
-
     // este método retorna o proprietário do caso de teste.
     public Owner getTestcaseOwner(Testcase testcase){}; 
 
