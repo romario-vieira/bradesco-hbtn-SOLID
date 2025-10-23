0721b7da0b9ecddbdeac177dcbb969c89dab232f
Romário Vieira <romario.sistemas@gmail.com>
2025-10-22 23:22:31 -0300

    clean code

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,30 @@
+public class Comments {
+
+    // Copyright (C) 2022 by CleanCode.
+    // Lançado sob os termos da GNU General Public License versão 2 ou posterior
+
+    // este método retorna o proprietário do caso de teste.
+    public Owner getTestcaseOwner(Testcase testcase){}; 
+
+
+    // retorna horas em mês
+    public Hours getHoursInQuarter(){}; 
+
+
+    // TODO Este método deve ser removido
+    public void check(){
+        return;
+    }
+
+
+    /**
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // alguma lógica de negócios aqui
+    }
+}
