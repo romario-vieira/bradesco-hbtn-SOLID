4de6d8c6cb6227299e18ae888d08cb73f269827e
Romário Vieira <romario.sistemas@gmail.com>
2025-10-22 23:24:25 -0300

    ajuste

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,5 +1,9 @@
 public class Comments {
 
+    // Copyright (C) 2022 by CleanCode.
+    // Lançado sob os termos da GNU General Public License versão 2 ou posterior
+
+
     // este método retorna o proprietário do caso de teste.
     public Owner getTestcaseOwner(Testcase testcase){}; 
 
@@ -8,10 +12,14 @@ public class Comments {
     public Hours getHoursInQuarter(){}; 
 
 
+
     // TODO Este método deve ser removido
     public void check(){
         return;
     }
+
+
+    // 3. Muita informação
 
 
     /**
